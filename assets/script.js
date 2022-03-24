a52e9f4f8e173a6a8dbdaa7b746811dfd83b3252
souhila boukari <suhamimisou14@gmail.com>
2022-03-23 22:35:14 -0400

    adding searchCity function to fetch the API and adding another function lettercase to let the user input the city name either upp or lower case

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,54 @@
 
 
 
+function searchCity() {
 
+    var cityName = letterCase($("#cityName")[0].value.trim());
+
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
+
+    fetch(apiURL).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+
+                $(".form-control")[0].textContent = cityName + " (" + moment().format('M/D/YYYY') + ")";
+
+                $("#city-list").append('button' + cityName);
+
+                const lat = data.coord.lat;
+                const lon = data.coord.lon;
+
+                var latLonPair = lat.toString() + " " + lon.toString();
+
+                localStorage.setItem(cityName, latLonPair);
+
+                apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
+
+                fetch(apiURL).then(function (newResponse) {
+                    if (newResponse.ok) {
+                        newResponse.json().then(function (newData) {
+                            getCurrentWeather(newData);
+                        })
+                    }
+                })
+            })
+        } else {
+            alert("Cannot find city!");
+        }
+    })
+
+
+}
+function letterCase(city) {
+    var updatedCity = city.LowerCase().split(" ");
+    var returnedCity = "";
+    for (var i = 0; i < updatedCity.length; i++) {
+        updatedCity[i] = updatedCity[i][0].UpperCase() + updatedCity[i].slice(1);
+        returnedCity += " " + updatedCity[i];
+    }
+    return returnedCity;
+}
+localStorage.clear();
 
 
 
